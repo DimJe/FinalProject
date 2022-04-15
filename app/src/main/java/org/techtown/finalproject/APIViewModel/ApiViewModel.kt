03344016a6b423c1cf8c6bfaa8650038807b4aac
Dimje <mark759@naver.com>
2022-04-15 14:19:30 +0900

    서버와 과제 데이터 통신 확인

--- app/src/main/java/org/techtown/finalproject/APIViewModel/ApiViewModel.kt
+++ app/src/main/java/org/techtown/finalproject/APIViewModel/ApiViewModel.kt
@@ -2,33 +2,45 @@ package org.techtown.finalproject.APIViewModel
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
+import okhttp3.OkHttpClient
 import org.techtown.finalproject.MainActivity.Companion.TAG
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
 
 class ApiViewModel : ViewModel() {
 
     var retrofit : Retrofit
     init {
+        val okHttpClient = OkHttpClient().newBuilder()
+            .connectTimeout(80, TimeUnit.SECONDS)
+            .readTimeout(80, TimeUnit.SECONDS)
+            .writeTimeout(80, TimeUnit.SECONDS)
+            .build()
         retrofit = Retrofit.Builder()
-            .baseUrl("http://54.221.144.212:8000/")
+            .baseUrl("http://54.242.168.73:8000/")
+            .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
     fun getTask(id:String,pw:String){
         val api = retrofit.create(GetTaskData::class.java)
-
+        Log.d(TAG, "getTask: called")
         val result = api.sendData(lmsItem(id,pw))
-        result.enqueue(object : Callback<String>{
-            override fun onResponse(call: Call<String>, response: Response<String>) {
+        result.enqueue(object : Callback<APIdata>{
+            override fun onResponse(call: Call<APIdata>, response: Response<APIdata>) {
                 Log.d(TAG,"CallAPI - onResponse() called")
-                Log.d(TAG, "onResponse: ${response.body()}")
+                response.body()!!.task.forEach{
+                    Log.d(TAG, "${it.title}")
+                }
+
 
             }
-            override fun onFailure(call: Call<String>, t: Throwable) {
+            override fun onFailure(call: Call<APIdata>, t: Throwable) {
                 Log.d(TAG,"CallAPI - onFailure() called ${t.localizedMessage}")
             }
         })
